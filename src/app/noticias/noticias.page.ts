ec41f52f6931c32367a0d2315ddb0eba1fab7cbf
Daniel Bueno Carriedo <danielbuenocarriedo@gmail.com>
2019-02-12 12:00:31 +0000

    Noticias basico

--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-noticias',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticias.page.scss'],
 })
 export class NoticiasPage implements OnInit {
-
-  constructor() { }
+  private noticias;
+  constructor(private http: HttpClient) {
+    this.http.get('https://papacria-dev-space-danielbueno.c9users.io/api/noticias').subscribe((response) => {
+      console.log(response);
+      this.noticias = response;
+    })
+   }
 
   ngOnInit() {
   }
