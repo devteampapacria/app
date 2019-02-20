9d31afb2fb33bc85ebd2a57e9050bba87cc1bf01
Ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-02-20 13:19:27 +0000

    mensajes casi terminado
    falta poner una animación al principio y hacer el page en laravel para no pedir 1203919283710237462037456234 objetos

--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -17,44 +17,43 @@ export class NoticiasPage implements OnInit {
     limite = 3;
     //indice de noticias que llevamos
     i = 0;
+
     constructor(private http: HttpClient) {
-        this.http.get('https://papacria-dev-space-danielbueno.c9users.io/api/noticias').subscribe((response) => {
-            console.log(response);
-            this.todas = response;
-            //por defecto ponemos el limite de noticias
-            for (this.i; this.i < this.limite; this.i++) {
-                this.noticias.push(this.todas[this.i]);
-            }
-        })
+        this.doRefresh(event);
     }
     //cuando se llame al evento de loaddata
     loadData(event) {
-        //incrementamos el limite en 3
-        this.limite = this.limite + 3;
-        //y recorremos hasta encontrar el limite
-        for (this.i; this.i < this.limite; this.i++) {
-            //en caso de que ya no queden pues sencillamente desactivo el evento y paro el bucle
-            if (this.todas[this.i] == undefined) {
-                event.target.disabled = true;
-                break;
-            }
-            this.noticias.push(this.todas[this.i]);
-        }
-    }
-    doRefresh(event) {
-        console.log('Begin async operation');
-        this.noticias = new Array;
-        this.i=0;
-        this.limite=3;
-        this.http.get('https://papacria-dev-space-danielbueno.c9users.io/api/noticias').subscribe((response) => {
-            console.log(response);
-            this.todas = response;
-            //por defecto ponemos el limite de noticias
+        setTimeout(() => {
+            //incrementamos el limite en 3
+            this.limite = this.limite + 3;
+            //y recorremos hasta encontrar el limite
             for (this.i; this.i < this.limite; this.i++) {
+                //en caso de que ya no queden pues sencillamente desactivo el evento y paro el bucle
+                if (this.todas[this.i] == undefined) {
+                    event.target.complete();
+                    break;
+                }
                 this.noticias.push(this.todas[this.i]);
             }
-        })
-        
+        }, 1000);
+    }
+    doRefresh(event) {
+        setTimeout(() => {
+            this.noticias = new Array;
+            this.i = 0;
+            this.limite = 3;
+            this.http.get('https://papacria-dev-space-danielbueno.c9users.io/api/noticias').subscribe((response) => {
+                this.todas = response;
+                //por defecto ponemos el limite de noticias
+                for (this.i; this.i < this.limite; this.i++) {
+                    this.noticias.push(this.todas[this.i]);
+                }
+                event.target.complete();
+
+            })
+        }, 1000);
+
+
     }
 
     ngOnInit() {
